e8f62994c3a33bdeac955f2564eb482d6ff360c5
Ari Sutarman <hansyah242@gmail.com>
2022-06-12 21:18:23 +0700

    Final

--- script.js
+++ script.js
@@ -1,24 +1,16 @@
-// Insert angka ke textview
 const insert = (param) => {
   document.form.textarea.value = document.form.textarea.value + param;
 };
 
-// Fungsi Hitung
 const equal = () => {
   var hasil = document.form.textarea.value;
   document.form.textarea.value = eval(hasil);
 };
 
-const insertSin = (sin) => {
-  document.form.textarea.value = sin + "( " + sin + " )";
-};
-
-// Clean
 const clean = () => {
   document.form.textarea.value = "";
 };
 
-// Delete
 const back = () => {
   var hasil = document.form.textarea.value;
   document.form.textarea.value = hasil.substring(0, hasil.length - 1);
